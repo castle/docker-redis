bd61b219744947b69bd7ac3dce6cd4eba62b0caa
Filip Tepper <filip@tepper.pl>
2019-02-27 12:47:43 +0100

    wip

--- redis-ssl/start.sh
+++ redis-ssl/start.sh
@@ -4,11 +4,18 @@ set -e
 
 cd /usr/local/etc/stunnel;
 
-./generate-key.sh
+# ./generate-key.sh
 
-chmod 600 rediscert.pem
+# chmod 600 rediscert.pem
 
-echo "connect = $HOST:6379" >> stunnel.conf
+redis-server /usr/local/etc/redis/redis.conf --port 6391 --cluster-config-file nodes-6391.conf
+redis-server /usr/local/etc/redis/redis.conf --port 6392 --cluster-config-file nodes-6392.conf
+redis-server /usr/local/etc/redis/redis.conf --port 6393 --cluster-config-file nodes-6393.conf
 
-redis-server /usr/local/etc/redis/redis.conf
-stunnel4 /usr/local/etc/stunnel/stunnel.conf
+echo "yes" | redis-cli --cluster create 127.0.0.1:6391 127.0.0.1:6392 127.0.0.1:6393
+
+# stunnel4 /usr/local/etc/stunnel/stunnel.conf
+
+while true; do
+  :
+done
